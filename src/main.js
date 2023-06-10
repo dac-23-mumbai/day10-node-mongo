db6bee24a26d748a03ecb142787fe0250037a4e7
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-10 11:36:37 +0530

    basic.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,3 @@
-import { MongoClient } from "mongodb";
-
-async function main() {
-  const uri = "mongodb://localhost:27017";
-  const client = new MongoClient(uri);
-
-  let db = client.db("mydb");
-  let messageCollection = db.collection("message");
-
-  let inputDocument = { message: "helloo" };
-  await messageCollection.insertOne(inputDocument);
-  console.log("sucess");
-
-  await client.close();
+function main() {
+  return "Hello World";
 }
