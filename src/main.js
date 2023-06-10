83c996f14327b014062f98913dc4669a4301a60b
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-10 10:18:42 +0530

    passing data as arugment.

--- src/main.js
+++ src/main.js
@@ -1,23 +1,25 @@
 import { MongoClient } from "mongodb";
 
-async function insertRecord() {
+async function insertRecord(jsonDocument) {
   const uri = "mongodb://localhost:27017";
-  // open the connection
   const client = new MongoClient(uri);
 
   const db = client.db("mydb");
   const message = db.collection("message");
 
-  let jsonDocument = { message: "Hello World", to: "santosh", from: "tejas" };
   await message.insertOne(jsonDocument);
 
-  // close the connection
   await client.close();
   console.log("Record Added");
 }
 
 async function main() {
-  await insertRecord();
+  let jsonDocument = {
+    message: "Hello Universee",
+    to: "santosh",
+    from: "tejas",
+  };
+  await insertRecord(jsonDocument);
 }
 
 main();
