144e7376f63322c1e3b1b9fece6f7943b426df3b
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-10 12:34:33 +0530

    node + express + mongo

--- src/main.js
+++ src/main.js
@@ -1,17 +1,37 @@
+import { MongoClient } from "mongodb";
 import express from "express";
 const app = express();
 
-function main(req, res) {
-  res.send("Hello");
+async function main(req, res) {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  const db = client.db("mydb");
+  const messageColl = db.collection("message");
+
+  let inputDocument = { message: "Hello Node Mongo Express " };
+  await messageColl.insertOne(inputDocument);
+
+  await client.close();
+
+  res.send("record added");
+}
+
+async function findAllMessage(req, res) {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  const db = client.db("mydb");
+  const messageColl = db.collection("message");
+
+  let list = await messageColl.find().toArray();
+
+  await client.close();
+  res.send(list);
 }
 
 app.get("/main", main);
-
-app.get("/first", (req, res) => res.send("Hello World"));
-
-app.get("/second", (req, res) => {
-  res.send("Hello SEcond!!");
-});
+app.get("/find", findAllMessage);
 
 // http://loaclhost:4000
 app.listen(4000);
