8c9d72161e1f6c011ade13d4b8d1a4b5ef8ecbf9
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-10 09:49:26 +0530

    hello node.

--- src/main.js
+++ src/main.js
@@ -2,14 +2,15 @@ import { MongoClient } from "mongodb";
 
 async function main() {
   const uri = "mongodb://localhost:27017";
+  // open the connection
   const client = new MongoClient(uri);
 
   const db = client.db("mydb");
   const message = db.collection("message");
-
   await message.insertOne({});
-  await client.close();
 
+  // close the connection
+  await client.close();
   console.log("Record Added");
 }
 
