c9a3251c972a2d9ddda23cb807820f4ca427f85c
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-10 09:44:37 +0530

    hello node + mongo

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
-import {} from "node:fs";
+import { MongoClient } from "mongodb";
 
-console.log("Hello");
+async function main() {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  const db = client.db("mydb");
+  const message = db.collection("message");
+
+  await message.insertOne({});
+  await client.close();
+
+  console.log("Record Added");
+}
+
+main();
