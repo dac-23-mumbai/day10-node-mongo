906063730440327361b1f35132a687809b3ebc4f
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-10 12:05:07 +0530

    hello world. using express.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-// remotely executable
-// Network call
-// Network Address
-// Remote address
-// http://localhost:4000/
 import express from "express";
 const app = express();
 
+function main(req, res) {
+  // return "some message";
+  res.send("some message...!")
+}
+
+// http://loaclhost:4000/main
+app.get("/main", main);
+
+// http://loaclhost:4000
 app.listen(4000);
