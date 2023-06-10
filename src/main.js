659bd612ab34d3d6df9d22ab79f335f6ee7f7d62
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-10 12:10:12 +0530

    adding more address, endpoint.

--- src/main.js
+++ src/main.js
@@ -3,11 +3,19 @@ const app = express();
 
 function main(req, res) {
   // return "some message";
-  res.send("some message...!")
+  res.send("some message...!");
+}
+
+function first(req, res) {
+  // logic...
+  res.send("Success");
 }
 
 // http://loaclhost:4000/main
 app.get("/main", main);
+
+// http://loaclhost:4000/first
+app.get("/first", first);
 
 // http://loaclhost:4000
 app.listen(4000);
